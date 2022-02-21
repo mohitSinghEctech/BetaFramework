12489558a00e9cdd47a9c4ad1f3273fd511eb6f9
Mohit Kumar Singh <mohit.kumar@nickelfox.co>
2022-02-21 21:37:01 +0530

    Initial Commit

---
+++ BetaFramework/BetaFramework.h
@@ -0,0 +1,18 @@
+//
+//  BetaFramework.h
+//  BetaFramework
+//
+//  Created by Mohit Kumar Singh on 21/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BetaFramework.
+FOUNDATION_EXPORT double BetaFrameworkVersionNumber;
+
+//! Project version string for BetaFramework.
+FOUNDATION_EXPORT const unsigned char BetaFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetaFramework/PublicHeader.h>
+
+
